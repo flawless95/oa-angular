0bdacf5a2d46f340697c75b2e8eeadf9392e1fcb
flawless95 <lcubin959@gmail.com>
2018-11-02 17:03:49 +0800

    revert

--- src/scope.js
+++ src/scope.js
@@ -32,15 +32,15 @@ Scope.prototype.$$digestOnce = function() {
   return dirty;
 };
 
-// Scope.prototype.$digest = function() {
-//   var ttl = 10;
-//   var dirty;
-//   do {
-//     dirty = this.$$digestOnce();
-//     if (dirty && !(ttl--)){
-//       throw '10 digest iterations reached';
-//     }
-//   } while(dirty);
-// };
+Scope.prototype.$digest = function() {
+  var ttl = 10;
+  var dirty;
+  do {
+    dirty = this.$$digestOnce();
+    if (dirty && !(ttl--)){
+      throw '10 digest iterations reached';
+    }
+  } while(dirty);
+};
 
 module.exports = Scope;
